a16174610256200d43a47e26cad5bad35c293d6d
vishumane <47922687+vishumane@users.noreply.github.com>
2021-08-25 16:00:45 +0530

    Update QrCode.jsx

--- pdf/src/qrcode/QrCode.jsx
+++ pdf/src/qrcode/QrCode.jsx
@@ -4,15 +4,21 @@ import {useState,useEffect} from 'react';
 
 const QrCode=({text})=> {
     const[src,setSrc]=useState('');
+    // const[txt,setTxt]=useState([]);
 
     useEffect(() => {
-      QRCode.toDataURL(text).then((setSrc));
+        // if(txt){
+        //     QRCode.toDataURL(txt).then((setSrc));
+        // }
+     
+        QRCode.toDataURL(text).then((setSrc));
     //     setSrc(data);
     //   })
     },[])
-
+    
     return (
         <div>
+            {/* <input type="text" onChange={(e)=>setTxt(e.target.value)}></input> */}
             <p>your Qrcode is available here for your data :<b>{text}</b></p>
             <img src={src}  alt="qrcode"/>
         </div>
